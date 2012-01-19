fc9011106dfd49a773eda505d94d28fb68b835e6
blangel <langelb@gmail.com>
2012-01-19 14:34:51 -0500

    * Making shell-args a property. * Augmenting Scripts documentation.

--- src/main/java/net/ocheyedan/ply/exec/ShellExec.java
+++ src/main/java/net/ocheyedan/ply/exec/ShellExec.java
@@ -24,15 +24,19 @@ public final class ShellExec {
     static Execution createShellExecutable(Execution execution, File configDirectory) {
         String executable = execution.executionArgs[0];
         String shell = Props.getValue(Context.named("ply"), "shell", configDirectory, execution.script.scope);
+        String shellArgs = Props.getValue(Context.named("ply"), "shell.args", configDirectory, execution.script.scope);
         if (shell.isEmpty()) {
             Output.print("^error^ Cannot run '^b^%s^r^'. No ^b^shell^r^ property defined (^b^ply set shell=xxxx in ply^r^).",
                     executable);
             throw new SystemExit(1);
         }
-        String[] args = new String[2 + execution.executionArgs.length];
+        int supplimentalArgLength = 1 + (shellArgs.isEmpty() ? 0 : 1);
+        String[] args = new String[supplimentalArgLength + execution.executionArgs.length];
         args[0] = shell;
-        args[1] = "-c";
-        System.arraycopy(execution.executionArgs, 0, args, 2, execution.executionArgs.length);
+        if (supplimentalArgLength > 1) {
+            args[1] = shellArgs;
+        }
+        System.arraycopy(execution.executionArgs, 0, args, supplimentalArgLength, execution.executionArgs.length);
         return execution.with(executable, args);
     }
 
